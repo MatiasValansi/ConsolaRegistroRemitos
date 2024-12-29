a3a27ed47440c27265fc66096e452d1c7d9a7dc7
MatiasValansi <valansimatias@gmail.com>
2024-12-29 17:35:07 -0300

    Proyecto Creado con Paquete Lógica

---
+++ RegistroRemitos2025/src/main/java/com/mycompany/registroremitos2025/RegistroRemitos2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registroremitos2025;
+
+/**
+ *
+ * @author matie
+ */
+public class RegistroRemitos2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
